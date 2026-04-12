6f42d1735fe690bc46b02bb87e9ede59faf4201b
csimal <cedric.simal@unamur.be>
2026-04-12 20:19:57 +0200

    Add msign and mcsgn

---
+++ src/mcsgn.jl
@@ -0,0 +1,22 @@
+
+struct MCsgnEigen end
+
+"""
+    mcsgn(A, method)
+
+Compute the matrix sign of a square matrix `A` using the algorithm `method`.
+
+For a square matrix `A`, the matrix sign `mcsgn(A)` is defined as
+```math
+mcsgn(A) = (A^2)^{-1/2} A = A (A^2)^{-1/2}
+```
+
+If `A` admits an eigendecomposition `A = P Λ P^{-1}`, then `mcsgn(A) = P sign.(Λ) P^{-1}`, i.e. `mcsgn(A)` has the same eigenvectors, but every eigenvalue is replaced by its sign.
+"""
+function mcsgn end
+
+mcsgn(A::AbstractMatrix, ns::AbstractNewtonSchulz) = newton_schulz_square(A, coeffs(ns))
+
+mcsgn(A::AbstractMatrix, ns::ClassicalNewtonSchulz) = newton_schulz_square(A, coeffs(ns), ns.nsteps)
+
+mcsgn(A::AbstractMatrix, ns::NSJordan) = newton_schulz_square(A, coeffs(ns), ns.nsteps)
--- src/msign.jl
+++ src/msign.jl
@@ -1,7 +1,26 @@
 
-struct MsignSVD end
+"""
+    msign(A, method)
 
-function msign(A::AbstractMatrix, ::MsignSVD)
-    U, S, V = svd(A)
-    return U * sign.(S) * V
+Compute the matrix sign of a rectangular matrix `A` using the algorithm `method`.
+
+For a rectangular matrix `A`, `msign(A)` is defined as
+```math
+msign(A) = (A * A^T)^{-1/2} A = A (A * A^T)^{-1/2} 
+```
+If `A = U * Σ * V^T` is the SVD decomposition of `A`, then `msign(A) = U sign.(Σ) * V^T`, i.e. `msign(A)` has the same singular vectors as `A` but any non-zero singular value is replaced by one.
+"""
+function msign end
+
+struct MSignSVD end
+
+function msign(A::AbstractMatrix, ::MSignSVD)
+    F = svd(A)
+    return F.U * Diagonal(sign.(F.S)) * F.Vt
 end
+
+msign(A::AbstractMatrix, ns::AbstractNewtonSchulz) = newton_schulz(A, coeffs(ns))
+
+msign(A::AbstractMatrix, ns::ClassicalNewtonSchulz) = newton_schulz(A, coeffs(ns), ns.nsteps)
+
+msign(A::AbstractMatrix, ns::NSJordan) = newton_schulz(A, coeffs(ns), ns.nsteps)
